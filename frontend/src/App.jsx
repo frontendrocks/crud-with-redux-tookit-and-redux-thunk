f2c84b4401031a568d30eeae563caa3547234191
frontendrocks <gp9141@gmail.com>
2024-06-04 00:07:52 +0530

    crud with redux react reacttoolkit and redux thunk

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+// frontend/src/App.jsx
+import { Box, Typography } from "@mui/material";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <Box>
+      <Box sx={{ display: "flex", justifyContent: "center", mt: 8 }}>
+        <Typography variant="h5">
+          CRUD operation using React JS & Redux Toolkit
+        </Typography>
+      </Box>
+      <Home />
+    </Box>
+  );
+};
+
+export default App;
